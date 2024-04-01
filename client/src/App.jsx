fde2ded0234e89e3e706290437072d60cadac68e
Suvakshya Acharya <asuvaksha@gmail.com>
2024-04-01 21:36:01 +0545

    "second commit"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import "./App.css";
-import Layout from "./components/Layout/Layout";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <Layout>
-        <h1>Ecommerce app</h1>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
   );
 }
